42d8dc18249d63cde291e1d64c1232ef00046954
BIT <BIT@DESKTOP-VC61ESU>
2019-05-29 15:50:21 +0900

    java config

--- src/main/java/com/cafe24/mysite/config/WebConfig.java
+++ src/main/java/com/cafe24/mysite/config/WebConfig.java
@@ -1,24 +1,18 @@
 package com.cafe24.mysite.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.ViewResolver;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.Import;
+
+import com.cafe24.config.web.FileuploadConfig;
+import com.cafe24.config.web.MVCConfig;
+import com.cafe24.config.web.MessageConfig;
+import com.cafe24.config.web.SecurityConfig;
 
 @Configuration
-@EnableWebMvc
+@EnableAspectJAutoProxy
 @ComponentScan({"com.cafe24.mysite.controller"})
+@Import({SecurityConfig.class, FileuploadConfig.class, MVCConfig.class, MessageConfig.class})
 public class WebConfig {
-	
-	@Bean
-	public ViewResolver viewResolver() {
-		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-		resolver.setPrefix("/WEB-INF/views/");
-		resolver.setSuffix(".jsp");
-		resolver.setExposeContextBeansAsAttributes(true);
-
-		return resolver;
-	}
 }
